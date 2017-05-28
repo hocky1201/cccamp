29a154b5b1462cd174caa2d36f65abee364d7278
jinwenyang <amyyhr1211@gmail.com>
2017-05-28 22:32:30 +0800

    no message

---
+++ allcamp.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <?php include "header.php"; ?>
+<?php
+ include "dbconnect.php";
+
+ $result=mysqli_query($link,"SELECT * FROM activity");
+
+echo "<table border=1>";
+
+
+while($row=mysqli_fetch_assoc($result)){
+        echo"<tr>";
+        echo "<td>"; echo "海報位置"; echo "</td>";
+echo "<td>";
+echo "營隊名稱:".$row["act_name"]."<br>";
+echo "價格:".$row["act_price"]."<br>";
+echo "縣市:".$row["act_area"]."<br>";
+echo "招生對象:".$row["act_stage"]."<br>";
+echo "營隊類型:".$row["act_field"]."<br>";
+echo "報名時段:".$row["act_signup_starttime"];
+echo "~".$row["act_signup_endtime"]."<br>";
+echo "活動時間:".$row["act_starttime"];
+echo "~".$row["act_endtime"]."<br>";
+echo "負責人:".$row["act_contact_person"];
+echo "(".$row["act_contact_phone"].")<br>";
+echo "描述:".$row["act_desc"]."<br>";
+echo "</td>";
+
+
+
+}
+echo"<table>";
+
+
+
+mysqli_close($link);
+
+
+?>
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,80 +1,58 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../lity/lity.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jcarousel/0.3.5/jquery.jcarousel.js"></script>
-    <script src="../lity/lity.min.js"></script>
-    <script src="../js/all.js"></script>
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-     <div class="quick-navbar">
-        
-                <?php
-                    if(isset($_SESSION['user'])){
-                        echo "<span class='hello-notify'>".$_SESSION['user']."，歡迎來到CCCamp，點此<a href='logout.php'>登出</a></span>";
-                    }else{
-                        echo "<span class='hello-notify'>歡迎來到CCCamp，請<a href='login.php'>登入</a>，或是<a href='regist.php'>註冊會員</a></span>";
-                    }
-                ?>
+    <div class="quickbar">
 
-                <?php
-                    if(isset($_SESSION['auth'])){
-                        if($_SESSION['auth'] == 1) {
-                            echo "<ul class='quick-link'>
-                                    <li><a href=''>quick-navbar1</a></li>
-                                    <li>|</li>
-                                    <li><a href=''>quick-navbar2</a></li>
-                                    <li>|</li>
-                                    <li><a href=''>quick-navbar3</a></li>
-                                    <li>|</li>
-                                    <li><a href=''>quick-navbar4</a></li>
-                                 </ul>";
-                        }elseif ($_SESSION['auth'] == 2) {
-                            echo "<ul class='quick-link'>
-                                    <li><a href=''>quick-navbar1</a></li>
-                                    <li>|</li>
-                                    <li><a href=''>quick-navbar2</a></li>
-                                    <li>|</li>
-                                    <li><a href=''>quick-navbar3</a></li>
-                                    <li>|</li>
-                                    <li><a href=''>quick-navbar4</a></li>
-                                 </ul>";
-                        }
-                    }else {
-                        echo "<ul class='quick-link'>
-                                <li><a href=''>營隊一覽</a></li>
-                                <li>|</li>
-                                <li><a href=''>報名管理</a></li>
-                                <li>|</li>
-                                <li><a href=''>營隊申請</a></li>
-                                <li>|</li>
-                                <li><a href='./supplier/'>廠商專區</a></li>
-                             </ul>";
-                    }
-                ?>
-                <div class="clear"></div>
-                <form action="search.php" method="get"><input type="text" placeholder="搜尋營隊"><input type="submit" value="搜尋"></form>
-            </div>
+        <div class="quicklogin">
+        <?php
+        if(isset($_SESSION['user'])) {
+            echo "<b>".$_SESSION['user']."</b> 你好，點此<a href='logout.php'>登出</a>";
+        }else {
+            echo "歡迎來到CCCamp，請<a href='login.php'>登入</a>，或是<a href='regist.php'>註冊會員</a></span>";
+        }
+        ?>
+        </div>
 
-            <banner>
-                    <div class="jcarousel">
-                        <ul>
-                            <li><a href=""><img src="http://clipart-library.com/images/pT5rG4Xpc.png" alt="BannerImage" ></a></li>
-                            <li><a href=""><img src="http://clipart-library.com/images/pT5rG4Xpc.png" alt="BannerImage" ></a></li>
-                            <li><a href=""><img src="http://clipart-library.com/images/pT5rG4Xpc.png" alt="BannerImage" ></a></li>
-                            <li><a href=""><img src="http://clipart-library.com/images/pT5rG4Xpc.png" alt="BannerImage" ></a></li>
-                        </ul>
-                    </div>
-                    
-                    <a class="jcarousel-prev" href="#"><</a>
-                    <a class="jcarousel-next" href="#">></a>
-            </banner>
-            <div class="clear"></div>
+        <div class="searchbar">
+            <form action="search.php" method="get">
+                <input type="text" name="srch" placeholder="搜尋營隊">
+                <input type="submit" value="立即搜尋">
+            </form>
+        </div>
+
+        <div class="quicknav">
+        <?php
+        if(!isset($_SESSION['auth'])) {
+            echo "<ul>
+                <li><a href='allcamp.php'>營隊一覽</a></li><li>|</li>
+                <li><a href='user.php'>會員專區</a></li><li>|</li>
+                <li><a href='sup.php'>廠商專區</a></li>
+                </ul>";
+        }elseif ($_SESSION['auth'] == 'user') {
+            echo "<ul>
+                <li><a href=''>會員資料修改</a></li><li>|</li>
+                <li><a href=''>營隊收藏</a></li><li>|</li>
+                <li><a href=''>我的營隊收藏</a></li>
+                </ul>";
+        }elseif ($_SESSION['auth'] == 'admin') {
+            echo "<ul>
+                <li><a href=''>修改個人資料</a></li><li>|</li>
+                <li><a href=''>刊登營隊</a></li><li>|</li>
+                <li><a href=''>編輯營隊</a></li><li>|</li>
+                </ul>";
+        }
+        ?>
+        </div>
+
+    </div>
+    
 </body>
 </html>
