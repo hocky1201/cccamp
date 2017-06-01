df3b4b35392edbbcfc0631519cce69abe481ee94
jinwenyang <amyyhr1211@gmail.com>
2017-06-02 03:17:19 +0800

    Add files via upload
    可傳活動、使用者編號 收藏可點選或取消

--- allcamp.php
+++ allcamp.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,52 +6,52 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-    <div class="container">
-        
-        <?php 
-            include "header.php"; 
-            
-            include "dbconnect.php";
+    <?php include "header.php"; ?>
+<?php
+ include "dbconnect.php";
 
-            $result=mysqli_query($link,"SELECT * FROM activity");
+ $result=mysqli_query($link,"SELECT * FROM activity");
 
-            echo "<table border=1>";
+echo "<table border=1>";
 
 
-            while($row=mysqli_fetch_assoc($result)){
-                echo"<tr>";
-                echo "<td>"; echo "海報位置"; echo "</td>";
-                echo "<td>";
-                echo "營隊名稱:".$row["act_name"]."<br>";
-                $_SESSION['act_code'] = $row['act_code'];
-                $org=$row["act_ORG"];
-                echo "舉辦機關:<a href='supIntro.php'>$org</a><br>";
-                echo "報名費:".$row["act_price"]."<br>";
-                echo "縣市:".$row["act_area"]."<br>";
-                echo "招生對象:".$row["act_stage1"].$row["act_stage2"].$row["act_stage3"].$row["act_stage4"].$row["act_stage5"].$row["act_stage6"].$row["act_stage7"].$row["act_stage8"]."<br>";
-                echo "營隊類型:".$row["act_field"]."<br>";
-                echo "報名時段:".$row["act_signup_starttime"];
-                echo "~".$row["act_signup_endtime"]."<br>";
-                echo "活動時間:".$row["act_starttime"];
-                echo "~".$row["act_endtime"]."<br>";
-                echo "負責人:".$row["act_PICname"];
-                echo "(".$row["act_PICphone"].")<br>";
-                echo "描述:".$row["act_desc"]."<br>";
-                $url=$row["act_url"];
-                echo "詳細資訊:<a href='$url'>$url</a><br>";
-                echo "</td>";
-            }
-            echo"<table>";
+while($row=mysqli_fetch_assoc($result)){
+        echo"<tr>";
+        echo "<td>"; echo "海報位置"; echo "</td>";
+echo "<td>";
+echo "營隊名稱:".$row["act_name"]."<br>";
+$act_name=$row["act_name"];
+$act_code= $row['act_code'];
+$org=$row["act_ORG"];
+echo "舉辦機關:<a href='supIntro.php?sact_code=$act_code && sact_name=$act_name'>$org</a><br>";
+echo "報名費:".$row["act_price"]."<br>";
+echo "縣市:".$row["act_area"]."<br>";
+echo "招生對象:".$row["act_stage1"].$row["act_stage2"].$row["act_stage3"].$row["act_stage4"].$row["act_stage5"].$row["act_stage6"].$row["act_stage7"].$row["act_stage8"]."<br>";
+echo "營隊類型:".$row["act_field"]."<br>";
+echo "報名時段:".$row["act_signup_starttime"];
+echo "~".$row["act_signup_endtime"]."<br>";
+echo "活動時間:".$row["act_starttime"];
+echo "~".$row["act_endtime"]."<br>";
+echo "負責人:".$row["act_PICname"];
+echo "(".$row["act_PICphone"].")<br>";
+echo "描述:".$row["act_desc"]."<br>";
+$url=$row["act_url"];
+echo "詳細資訊:<a href='$url'>$url</a><br>";
+echo "</td>";
 
-            mysqli_close($link);
-        
-        ?>
 
-    </div>
+
+}
+echo"<table>";
+
+
+
+mysqli_close($link);
+
+
+?>
 </body>
 </html>
--- supIntro.php
+++ supIntro.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,9 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
     <?php include "header.php"; ?>
@@ -26,27 +25,71 @@ if (isset($_SESSION['auth'])){
 			
 }
 
-$act_code=$_SESSION['act_code'];
 
+$act_code=$_GET["sact_code"];
+$act_name=$_GET['sact_name'];
+echo "<h4>營隊:".$act_name."<h4>";
+$result=mysqli_query($link,"SELECT * FROM comment WHERE act_code = $act_code ; ");
+
+echo'<h4>評價</h4><br/>';
+echo "<table border=1>";
+
+while($row=mysqli_fetch_assoc($result)){    
+echo"<tr>";
+echo "<td>";
+echo "內容:".$row["c_content"]."<br>";
+echo "</td>";
+    }
+echo"<table>";
+
+$result=mysqli_query($link,"SELECT COUNT(col_code) AS ii FROM collect WHERE col_record ='收藏' GROUP BY act_code = $act_code AND u_code= $u_code; ");
+
+while($row=mysqli_fetch_assoc($result)){
+    $ii=$row["ii"];
+   
+}
 
 
 echo '<form method="post" action="supIntro.php" >';
-echo'<h4>評價</h4><br/>';
-echo'請輸入評價:<textarea name="c_content" rows="6" cols="50" placeholder="限300字"></textarea>';
-echo'<input type="submit">';
+echo'<h4>給評價</h4><br/>';
+echo'請輸入評價:<textarea name="c_content" rows="6" cols="50" placeholder="限300字"></textarea><br/><br/>';
+if($ii%2==0){
+echo'<input type="checkbox" name="col_record" vaLue="收藏">收藏<br/><br/>';
+}else{
+    echo'<input type="checkbox" name="col_record" vaLue="收藏">收藏(您已收藏，再勾選將取消收藏!!)<br/><br/>';
+}
+echo'<input type="hidden" name="act_code" value="'.$act_code.'" />';
+echo'<input type="submit" name="subb">';
+
 echo '</form>';
 
 $c_content=$_POST["c_content"];
+$col_record=$_POST["col_record"];
+$act_code=$_POST["act_code"];
 
+if(isset($_POST['subb'])) {
 
 $sql3="INSERT INTO comment (act_code, u_code, c_content) 
 VALUES ('$act_code', '$u_code','$c_content')";
+
+$sql4="INSERT INTO collect (act_code, u_code, col_record) 
+VALUES ('$act_code', '$u_code','$col_record')";
+
+
+
 $result=mysqli_query($link, $sql3);
+$result=mysqli_query($link, $sql4);
+
+
+
+
 
 
 mysqli_close($link);
 
-
+unset($_SESSION["act_code"]);
+header('Location: allcamp.php');
+}
 ?>
 </body>
 </html>
