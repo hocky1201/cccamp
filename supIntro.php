a0a392e1b62b32c132f9d447d2643dab17655601
jinwenyang <amyyhr1211@gmail.com>
2017-06-05 00:14:53 +0800

    0605改正提交空值錯誤

--- supIntro.php
+++ supIntro.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,58 +21,116 @@ if (isset($_SESSION['auth'])){
 	$checka = mysqli_query($link, "SELECT * FROM `user` WHERE `u_id` = '$finda' ; ");
     $rowFa = mysqli_fetch_assoc($checka);
         if(isset($rowFa)) 
-            $u_code= $rowFa['u_code'];
+            $usu_code= $rowFa['u_code'];
 			
 }
 
-$act_code=$_SESSION['act_code'];
+if(isset($_POST['subb'])) {
+
+    $c_content=$_POST["c_content"];
+    $col_record=$_POST["col_record"];
+    $act_code=$_POST["act_code"];
 
 
+    $sql3="INSERT INTO comment (act_code, u_code, c_content) 
+    VALUES ('$act_code', '$usu_code','$c_content')";
 
-echo'<h4>評價</h4><br/>';
+    $result=mysqli_query($link, $sql3);
 
-$result=mysqli_query($link,"SELECT * FROM comment WHERE act_code = $act_code ; ");
+    if(isset($_POST["col_record"])){
 
-echo "<table border=1>";
+    $sql4="INSERT INTO collect (act_code, u_code, col_record) 
+    VALUES ('$act_code', '$usu_code','$col_record')";
 
 
-while($row=mysqli_fetch_assoc($result)){
-        echo"<tr>";
-        
-echo "<td>";
-echo "內容:".$row["c_content"]."<br>";
-echo "</td>";
+    $result=mysqli_query($link, $sql4);
+    }
+    mysqli_close($link);
 
+    if(isset($_POST['subb'])){
+    unset($_POST['subb']);
+    }
+    header('Location: allcamp.php');
+
+}else{
+
+    $act_code=$_GET["sact_code"];
+    $act_name=$_GET['sact_name'];
+
+    $result=mysqli_query($link,"Select * From activity Where act_code=$act_code; ");
+    while($row=mysqli_fetch_assoc($result)){    
+    $u_code=$row["u_code"];
+    $act_ORG=$row["act_ORG"];
         }
-echo"<table>";
+
+    echo "<h4>營隊名稱:".$act_name."<h4>";
+
+
+    echo'<h4>對'.$act_ORG.'的評價:</h4><br/>';
+
+    $result=mysqli_query($link,"Select comment.c_content From comment,activity Where comment.act_code = activity.act_code And activity.u_code = $u_code; ");
+
+    echo "<table border=1>";
+
+    while($row=mysqli_fetch_assoc($result)){    
+    echo"<tr>";
+    echo "<td>";
+    echo "內容:".$row["c_content"]."<br>";
+    echo "</td>";
+            }
+    echo"<table>";
+
+    if(isset($_SESSION['auth'])){
+
+
+    $findc = mysqli_query($link, "SELECT * FROM `collect` WHERE u_code = $usu_code AND act_code= $act_code; ");
+
+
+    while($rowc = mysqli_fetch_assoc($findc)){
+    $col_code= $rowc['col_code'];
+    $col_record= $rowc['col_record'];
+        }
 
 
 
-echo '<form method="post" action="supIntro.php" >';
-echo'<h4>給評價</h4><br/>';
-echo'請輸入評價:<textarea name="c_content" rows="6" cols="50" placeholder="限300字"></textarea><br/><br/>';
-echo'<input type="radio" name="col_record" vaLue="收藏">收藏<br/><br/>';
-echo'<input type="submit">';
-echo '</form>';
 
-$c_content=$_POST["c_content"];
-$col_record=$_POST["col_record"];
+        if(!isset($col_code)){
+        echo '<form method="post" action="supIntro.php" >';
+        echo'<h4>給評價</h4><br/>';
+        echo'請輸入評價:<textarea name="c_content" rows="6" cols="50" placeholder="限300字"></textarea><br/><br/>';
 
+        echo'<input type="checkbox" name="col_record" vaLue="收藏">收藏營隊<br/><br/>';
+        echo'<input type="hidden" name="act_code" value="'.$act_code.'" />';
+        echo'<input type="submit" name="subb">';
 
-$sql3="INSERT INTO comment (act_code, u_code, c_content) 
-VALUES ('$act_code', '$u_code','$c_content')";
+        echo '</form>';
 
-$sql4="INSERT INTO collect (act_code, u_code, col_record) 
-VALUES ('$act_code', '$u_code','$col_record')";
+        mysqli_close($link);
 
 
+    }else if($col_record="收藏"){
 
-$result=mysqli_query($link, $sql3);
-$result=mysqli_query($link, $sql4);
+        echo '<form method="post" action="supIntroexe.php" >';
+        echo'<h4>給評價</h4><br/>';
+        echo'請輸入評價:<textarea name="c_content" rows="6" cols="50" placeholder="限300字"></textarea><br/><br/>';
+        echo'<input type="checkbox" name="col_record" vaLue="不收藏">收藏營隊(您已收藏，按下按鈕將取消收藏!!)<br/><br/>';
+        echo'<input type="hidden" name="act_code" value="'.$act_code.'" />';
+        echo'<input type="hidden" name="col_code" value="'.$col_code.'" />';
+        echo'<input type="submit" name="subb">';
+        echo '</form>';
 
-mysqli_close($link);
+
+        mysqli_close($link);
+
+                }
 
 
+    }else{
+
+    mysqli_close($link);
+
+    }
+}
 ?>
 </body>
 </html>
